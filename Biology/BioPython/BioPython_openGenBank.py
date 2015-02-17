93c23dc6240cadd527cd251b4aefa0596d1b928e
GinkoDante <ginkodante@gmail.com>
2015-02-17 17:07:36 -0500

    Revert "Revert "Revert "Initial Python Testing"""
    This reverts commit 07fa5938cc7ed3c435218e6f71b1db8a855dc2c7.

--- Biology/BioPython/BioPython_openGenBank.py
+++
@@ -1,6 +0,0 @@
-from Bio import SeqIO
-
-for seqRecord in SeqIO.parse("ls_orchid.gbk", "genbank"):
-    print(seqRecord.id)
-    print(repr(seqRecord.seq))
-    print(len(seqRecord))
--- Biology/BioSQL/SubDatabase_Create.py
+++
@@ -1,6 +0,0 @@
-from BioSQL import BioSeqDatabase
-
-server = BioSeqDatabase.open_database(driver="MySQLdb", user="root", passwd="", host="localhost", db="bioseqdb")
-
-db = server.new_database("orchids", description="Just to Test")
-server.commit()
